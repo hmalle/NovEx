b798ade544fad24606aaa89019c58c37b5537733
hmalle <hmalle90ak@gmail.com>
2018-02-06 13:59:57 -0800

    Added some css, maps can clear markers, and search the database for nearby coordinates and maybe drop on new markers! Some issues with the get profile

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,16 +5,23 @@
 //for initalizing google map!!
 //If I place it in document.ready, the initMap is called but it isnt known yet
 //before the document is ready.
-var map;
-function initMap(){
-  //console.log("The map is being initialized");
-  map = new google.maps.Map(document.getElementById('map'), {
+
+$(document).ready(function() {
+  var map;
+  var markerArray=[];
+  var service; 
+  function initMap(){
+    //console.log("The map is being initialized");
+    map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 37.8716, lng: -122.2727 }, //initially center it around berkeley!
       zoom: 9
     });
-}
-
-$(document).ready(function() {
+    service = new google.maps.places.AutocompleteService();
+  }
+  setTimeout(function(){
+    initMap();
+    initPage();
+  },1000);
 
 	// Initialize Firebase
 	var config = {
@@ -32,17 +39,31 @@ $(document).ready(function() {
 	// This function will be called any time a new profile is added to our database.
 	// We will do our jQuery call here to append profile divs (or cards if we are using bootstrap) to our html
   // index.html
-
-  //  database.ref().orderByChild("dateAdded").limitToLast(5).once("value", function(childSnapshot){
-	// 	var dataName = childSnapshot.val().name;
-	// 	var dataLoc = childSnapshot.val().loc;
-	// 	var dataTrade = childSnapshot.val().trade;
-	// 	var dataImg = childSnapshot.val().image;
-	// 	var dataBio = childSnapshot.val().bio;
-	// 	var dataContact = childSnapshot.val().contact;
-		
-	// 	$("#profiles").append();
-	// });
+  function initPage(){
+    database.ref().orderByChild("dateAdded").limitToLast(5).once("value", function(snapshot){
+      //Disaplays the first at the initial of the page TODO: Maybe this can be made more DRY 
+      // with the getExperts function below!!
+      //experts = JSON.stringify(snapshot);
+      snapshot.forEach(function(childSnapshot){
+        var prof = {
+          name: childSnapshot.val().name,
+          trade : childSnapshot.val().trade,
+          contact : childSnapshot.val().contact,
+          bio : childSnapshot.val().bio,
+        };
+        displayProfile(prof);
+        coordinates = childSnapshot.val().loc;
+        var marker = new google.maps.Marker({ 
+          animation: google.maps.Animation.DROP,
+          //draggable: true,
+          position: coordinates, 
+          map: map
+        });
+        markerArray.push(marker);
+        marker.setMap(map); //marker.setMap(null) //removes the marker!
+      });
+    },function(errorObject){ console.log("Errors handled: "+errorObject.code); });
+  }
 
 	// This function will be called every time the submit button is clicked on the create profile form
 	// Pushes the expert's newly created profile information to the database
@@ -51,32 +72,42 @@ $(document).ready(function() {
 	  // Prevents the page from reloading on click
     event.preventDefault();
     var expertName = $("#name").val().trim();
-    var expertLoc = getCoordinates($("#loc").val().trim());
+    var expertLoc ; //= getCoordinates($("#loc").val().trim());
     var expertTrade = $("#tradeInput").val();
     var expertImg = tempImage;
     var expertBio = $("#bio").val().trim();
     var expertContact = $("#contact").val().trim();
     console.log(expertName, expertLoc, expertTrade, expertImg, expertBio, expertContact)
     //
-    if(expertName != undefined && expertLoc != undefined && expertImg != undefined && expertBio != undefined && expertContact != undefined) {
-      database.ref().push({
-        name: expertName,
-        loc: expertLoc,
-        trade: expertTrade,
-        image: expertImg,
-        bio: expertBio,
-        contact: expertContact,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
-      });
-      console.log(expertName, expertLoc, expertTrade, expertImg, expertBio, expertContact);
-      // Clear the form text boxes after submit
-      $("#name").val(" ");
-      $("#loc").val(" ");
-      $("#tradeInput").val(" ");
-      $("#image").val(" ");
-      $("#bio").val(" ");
-      $("#contact").val(" ")
-    }
+    var placeId;
+    service.getQueryPredictions({input: $("#loc").val().trim() },function(predictions,status){
+      if(status != google.maps.places.PlacesServiceStatus.OK) { return; }
+      console.log(JSON.stringify(predictions[0].place_id));
+      placeId = predictions[0].place_id;
+    });
+    setTimeout( function(){
+      //wait 500milliseconds before calling getCoordinates
+      expertLoc = getCoordinates(placeId);
+      if(expertName != undefined && expertLoc != undefined && expertImg != undefined && expertBio != undefined && expertContact != undefined) {
+        database.ref().push({
+          name: expertName,
+          loc: expertLoc,
+          trade: expertTrade,
+          image: expertImg,
+          bio: expertBio,
+          contact: expertContact,
+          dateAdded: firebase.database.ServerValue.TIMESTAMP
+        });
+        console.log(expertName, expertLoc, expertTrade, expertImg, expertBio, expertContact);
+        // Clear the form text boxes after submit
+        $("#name").val(" ");
+        $("#loc").val(" ");
+        $("#tradeInput").val(" ");
+        $("#image").val(" ");
+        $("#bio").val(" ");
+        $("#contact").val(" ")
+      }
+    }, 500);
   });
     
   // This function will be called when the add-image button is clicked
@@ -109,16 +140,17 @@ $(document).ready(function() {
       if(coordinates){ //ignore displaying on the map if its undefined!!
         map = new google.maps.Map(document.getElementById('map'),{
           center: coordinates,
-          zoom: 10
+          zoom: 9
         });
         findExperts(tradeTitle, coordinates);
         displayJobs( tradeTitle, addr); //display jobs available in the area!!
+      }else{
+        console.log("No coordinates returned from your location input");
       }
     }, 500);
   });
 
   //----------------------------------------------------------------------------------
-  var service = new google.maps.places.AutocompleteService();
   //google maps stuff below here 
   //https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY 
   var placesBaseURL = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=";
@@ -133,7 +165,7 @@ $(document).ready(function() {
       $.ajax({ 
         url: geocodeBaseURL + placeId + mapsApiKey, 
         async:false,  //must wait for this function to complete before firing return! 
-        //timeout: 5000, //set timeout of 5 secs so we arent here forever!
+        timeout: 3000, //set timeout  so we arent here forever!
         method: "GET" 
       }).done(function(response){
         if(response.status === "OK"){ //then we have at least one result!!
@@ -144,15 +176,23 @@ $(document).ready(function() {
         }
       });
     console.log("Coordinates: "+JSON.stringify(coordinates));
-    return coordinates; //TODO: HOW ON EARTH CAN I DELAY THIS RETURN TILL THE CODE ABOVE IS DONE!!!!!
+    return coordinates; 
   }
 
   //---------------------------------------------------------------------------------- 
-  function findExperts (/*tradeTitle, coordinates*/){
+  function findExperts (tradeTitle, coordinates){
     //a function to match the tradeTitle and available experts in te specified region(coordinates)
     var experts;
+    //clear all markers off the map!!!
+    google.maps.Map.prototype.clearOverlays = function() {
+      for (var i = 0; i < markersArray.length; i++ ) {
+        markersArray[i].setMap(null);
+      }
+      markersArray.length = 0;
+    }
     database.ref().orderByChild("dateAdded").limitToLast(5).once("value", function(snapshot){
       //experts = JSON.stringify(snapshot);
+      $("#profiles").empty(); //empty the profile before appending new experts!!
       snapshot.forEach(function(childSnapshot){
         var prof = {
           name: childSnapshot.val().name,
@@ -160,76 +200,48 @@ $(document).ready(function() {
           contact : childSnapshot.val().contact,
           bio : childSnapshot.val().bio,
         };
-        displayProfile( prof );
-        coordinates = childSnapshot.val().loc;
-        var marker = new google.maps.Marker({ 
-          animation: google.maps.Animation.DROP,
-          //draggable: true,
-          position: coordinates, 
-          map: map
-        });
-        marker.setMap(map); //marker.setMap(null) //removes the marker!
+        var locate = childSnapshot.val().loc;
+        console.log("Coordinate: "+JSON.stringify(locate));
+        var latDiff = Math.abs(parseFloat(locate.lat) - parseFloat(coordinates.lat));
+        var lngDiff = Math.abs(parseFloat(locate.lng) - parseFloat(coordinates.lng));
+        console.log("lat , lng = " + latDiff + " , " + lngDiff);
+        if(latDiff <=0.5 && lngDiff <= 0.5){
+          displayProfile( prof );
+          var marker = new google.maps.Marker({ 
+            animation: google.maps.Animation.DROP,
+            //draggable: true,
+            position: locate, 
+            map: map
+          });
+          marker.setMap(map); //marker.setMap(null) //removes the marker!
+        }
       });
     },function(errorObject){ console.log("Errors handled: "+errorObject.code); });
   }
 
+  function clearMarkers(){
+    for(var a=0; a<markerArray.length; a++){
+      markerArray[a].setMap(null);
+    }
+    markerArray.length = 0;
+  }
   function displayProfile(prof){
     $("#profiles").append("<div class='card' id='prof'>"+
-      "<div class='card-header'>"+ prof.name +"</div>"+
+      //"<div class='card-header'>"+ prof.name +"</div>"+
       "<div class='card-body'>" +
         "<div class='col-sm-4 float-left'>" +  
-          "<img id='bioImg' src='http://lorempixel.com/150/150/'>" +
+          "<img id='bioImg' src='https://lorempixel.com/150/150/'>" +
         "</div>"+
         "<div class='col-sm-8 float-right'>"+
-          "<p> Trade: "+prof.trade +"</p>" +
-          "<p>contact: "+prof.contact+"</p>"+
-          "<p>Bio: "+prof.bio +"</p></div>" +
-        "</div>"+
-      "</div>");
-  }
-
-  function findExperts (/*tradeTitle, coordinates*/){
-    //a function to match the tradeTitle and available experts in te specified region(coordinates)
-    var experts;
-    database.ref().orderByChild("dateAdded").limitToLast(5).once("value", function(snapshot){
-      //experts = JSON.stringify(snapshot);
-      snapshot.forEach(function(childSnapshot){
-        var prof = {
-          name: childSnapshot.val().name,
-          trade : childSnapshot.val().trade,
-          contact : childSnapshot.val().contact,
-          bio : childSnapshot.val().bio,
-        };
-        displayProfile( prof );
-        coordinates = childSnapshot.val().loc;
-        var marker = new google.maps.Marker({ 
-          animation: google.maps.Animation.DROP,
-          //draggable: true,
-          position: coordinates, 
-          map: map
-        });
-        marker.setMap(map); //marker.setMap(null) //removes the marker!
-      });
-    },function(errorObject){ console.log("Errors handled: "+errorObject.code); });
-  }
-
-  function displayProfile(prof){
-    $("#profiles").append("<div class='card' id='prof'>"+
-      "<div class='card-header'>"+ prof.name +"</div>"+
-      "<div class='card-body'>" +
-        "<div class='col-sm-4 float-left'>" +  
-          "<img id='bioImg' src='http://lorempixel.com/150/150/'>" +
-        "</div>"+
-        "<div class='col-sm-8 float-right'>"+
-          "<p> Trade: "+prof.trade +"</p>" +
-          "<p>contact: "+prof.contact+"</p>"+
-          "<p>Bio: "+prof.bio +"</p></div>" +
+          "<p>Name   :"+prof.name+"</p>"+
+          "<p>Trade  :"+prof.trade +"</p>" +
+          "<p>Contact:"+prof.contact+"</p>"+
+          "<p>Bio    :"+prof.bio +"</p></div>" +
         "</div>"+
       "</div>");
   }
 
   function displayJobs (tradeTitle, coordinates){
-    console.log("displaying jobs using some job search API");
     console.log(coordinates.lat);
     var queryURL = "https://jobs.github.com/positions.json?lat=37.3229978&long=-122.0321823&description=intern&markdown=true";
     var proxy = 'https://cors-anywhere.herokuapp.com/';
