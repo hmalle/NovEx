6061c621bd2e9d2f939bd0787b9160a7f9c8ca4e
hmalle <hmalle90ak@gmail.com>
2018-02-07 12:07:31 -0800

    fixed maps issues and search by trade, other things...

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,20 +1,10 @@
 
-
-//TODO: Error in the deployed page where
-
-//for initalizing google map!!
-//If I place it in document.ready, the initMap is called but it isnt known yet
-//before the document is ready.
-
-
 $(document).ready(function() {
-
 
   var map;
   var markerArray=[];
   var service = new google.maps.places.AutocompleteService();
   function initMap(){
-
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 37.8716, lng: -122.2727 }, //initially center it around berkeley!
       zoom: 9
@@ -34,21 +24,13 @@ $(document).ready(function() {
 
 	firebase.initializeApp(config);
 	var database = firebase.database();
-  //a little delay for the database and maps to be loaded!! TODO: Is this necessary? 
-  //TODO: Sometimes I get initMap is not defined till I reload the page again!!
   setTimeout(function(){
     initMap();
     initPage();
   },1000);
 
-	// This function will be called any time a new profile is added to our database.
-	// We will do our jQuery call here to append profile divs (or cards if we are using bootstrap) to our html
-  // index.html
   function initPage(){
     database.ref().orderByChild("dateAdded").limitToLast(5).once("value", function(snapshot){
-      //Disaplays the first at the initial of the page TODO: Maybe this can be made more DRY 
-      // with the getExperts function below!!
-      //experts = JSON.stringify(snapshot);
       snapshot.forEach(function(childSnapshot){
         var prof = {
           name: childSnapshot.val().name,
@@ -70,9 +52,6 @@ $(document).ready(function() {
     },function(errorObject){ console.log("Errors handled: "+errorObject.code); });
   }
 
-	// This function will be called every time the submit button is clicked on the create profile form
-	// Pushes the expert's newly created profile information to the database
-  // profile.html
 	$("#add-profile").on("click", function(event){
 	  // Prevents the page from reloading on click
     event.preventDefault();
@@ -86,47 +65,29 @@ $(document).ready(function() {
     service.getQueryPredictions({input: $("#loc").val().trim() },function(predictions,status){
       if(status != google.maps.places.PlacesServiceStatus.OK) { return; }
       placeId = predictions[0].place_id;
-
     });
     setTimeout( function(){
       //wait 500milliseconds before calling getCoordinates
       expertLoc = getCoordinates(placeId);
-
       if(expertName != undefined && expertLoc != undefined && expertBio != undefined && expertContact != undefined) {
-
         database.ref().push({
           name: expertName,
           loc: expertLoc,
           trade: expertTrade,
-
           bio: expertBio,
           contact: expertContact,
           dateAdded: firebase.database.ServerValue.TIMESTAMP
         });
-
         // Clear the form text boxes after submit
         $("#name").val(" ");
         $("#loc").val(" ");
         $("#tradeInput").val(" ");
-
         $("#bio").val(" ");
         $("#contact").val(" ")
       }
     }, 500);
   });
-    
-  // This function will be called when the add-image button is clicked
-  // Takes an image from the user and saves it as a value
-  // profile.html
-  $("#add-image").on("click", function(openDialog){
-    openDialog.preventDefault();
-    tempImage = $("#fileid").click();
-    //console.log(tempImage);
-  });
-
-  // This function will be called when the user clicks the search button
-  // Searches database for conditionals and appends profile cards to #profiles div
-  // index.html
+ 
   $("#searchBtn").on("click", function(ev) {
     ev.preventDefault();
     var addr = $("#loc").val().trim();
@@ -158,30 +119,26 @@ $(document).ready(function() {
     $("#validate").parsley();
   });
 
-  //----------------------------------------------------------------------------------
   //google maps stuff below here 
-  //https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY 
-  var placesBaseURL = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=";
+  var placesBaseURL = "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=";//Didnt really use this!
   var geocodeBaseURL = "https://maps.googleapis.com/maps/api/geocode/json?place_id="; 
   var mapsApiKey = "&key=AIzaSyAnBhiFh5vRGwz9cQ9eBX2lhFszC_e1jrA";
   //https://maps.googleapis.com/maps/api/place/autocomplete/output?parameters
   function getCoordinates(placeId){
     var coordinates;
     //gets the coordinates from an address!
-      $.ajax({ 
-        url: geocodeBaseURL + placeId + mapsApiKey, 
-        async:false,  //must wait for this function to complete before firing return! 
-        timeout: 3000, //set timeout  so we arent here forever!
-        method: "GET" 
-      }).done(function(response){
-        if(response.status === "OK"){ //then we have at least one result!!
-          coordinates = response.results[0].geometry.location;
-          //console.log(coordinates);
-        }else{
-          console.log("Error: No coordinates were found for the searched address");
-        }
-      });
-    //console.log("Coordinates: "+JSON.stringify(coordinates));
+    $.ajax({ 
+      url: geocodeBaseURL + placeId + mapsApiKey, 
+      async:false,  //must wait for this function to complete before firing return! 
+      timeout: 3000, //set timeout  so we arent here forever!
+      method: "GET" 
+    }).done(function(response){
+      if(response.status === "OK"){ //then we have at least one result!!
+        coordinates = response.results[0].geometry.location;
+      }else{
+        console.log("Error: No coordinates were found for the searched address");
+      }
+    });
     return coordinates; 
   }
 
@@ -211,14 +168,16 @@ $(document).ready(function() {
         var lngDiff = Math.abs(parseFloat(locate.lng) - parseFloat(coordinates.lng));
         //console.log("latDiff , lngDiff = " + latDiff + " , " + lngDiff);
         if(latDiff <=0.5 && lngDiff <= 0.5){
-          displayProfile( prof );
-          var marker = new google.maps.Marker({ 
-            animation: google.maps.Animation.DROP,
-            //draggable: true,
-            position: locate, 
-            map: map
-          });
-          marker.setMap(map); //marker.setMap(null) //removes the marker!
+          if(prof.trade === tradeTitle){
+            displayProfile( prof );
+            var marker = new google.maps.Marker({ 
+              animation: google.maps.Animation.DROP,
+              //draggable: true,
+              position: locate, 
+              map: map
+            });
+            marker.setMap(map); //marker.setMap(null) //removes the marker!
+          }
         }
       });
     },function(errorObject){ console.log("Errors handled: "+errorObject.code); });
@@ -230,6 +189,7 @@ $(document).ready(function() {
     }
     markerArray.length = 0;
   }
+
   function displayProfile(prof){
     var pic_arr = ["http://donnabertaccini.com/wp-content/uploads/sites/2/2017/09/dmb-passport.jpg",
                     "http://photos1.blogger.com/blogger/6971/3344/1600/11.0.jpg",
@@ -241,7 +201,6 @@ $(document).ready(function() {
       "<div class='card-body'>" +
         "<div class='col-sm-4 float-left'>" + 
           "<img id='bioImg' src='"+pic_arr[selector]+"'>" +
-
         "</div>"+
         "<div class='col-sm-8 float-right'>"+
           "<p>Name   :"+prof.name+"</p>"+
@@ -265,7 +224,6 @@ $(document).ready(function() {
     }).done(function(response){
       console.log(response.length)
       for (i = 0; i < response.length && i < 6; i++) {
-
         var shortDescription = jQuery.trim(response[i].description).substring(0, 600);
         $("#jobs").append("<div class='card mb-3'>"+
          "<div class='card-header h2'>"+ response[i].title +"</div>"+
@@ -273,7 +231,6 @@ $(document).ready(function() {
           "<p class='h4'>Location: </p>"+ "<p>" + response[i].location + "</p>" +
           "<p class='h4'>Company: </p>" + "<p>" + response[i].company + "</p>" + 
           "<p class='h4'>Job Information: </p>" + shortDescription + "<span>. . .</span>" + "<br><br>" +
-
           "<p class='h4'>Apply Now: </p>" +
           response[i].how_to_apply + "</div>"
          );
